8322ba270ee953ed30fc4147ed4f1c120a3d0fae
uhtetaungkyaw <htetaungkyaw372005@gmail.com>
2024-02-26 14:26:36 +0630

    Initial commit

---
+++ src/main/java/org/ecommerce/sharkecommerce/SharkEcommerceApplication.java
@@ -0,0 +1,13 @@
+package org.ecommerce.sharkecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharkEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharkEcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ecommerce/sharkecommerce/SharkEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ecommerce.sharkecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharkEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
