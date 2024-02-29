ad84b6c7915fa0cb67f629ae25ffc1c7d2fd9267
uhtetaungkyaw <htetaungkyaw372005@gmail.com>
2024-02-29 13:13:12 +0630

    update code

--- src/main/java/org/ecommerce/sharkecommerce/repo/OrderRepo.java
+++ src/main/java/org/ecommerce/sharkecommerce/repo/OrderRepo.java
@@ -1,9 +1,10 @@
 package org.ecommerce.sharkecommerce.repo;
 
+import org.ecommerce.sharkecommerce.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepo extends JpaRepository {
+public interface OrderRepo extends JpaRepository<Order, Integer> {
 
 }
